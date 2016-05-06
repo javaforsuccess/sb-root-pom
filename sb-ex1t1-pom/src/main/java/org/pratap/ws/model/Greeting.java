92f6d69ed67940e8fb174ddcd6e95ab6e30709c1
Satendra Pratap Singh <org.pratap@gmail.com>
2016-05-06 23:36:13 +0530

    initial commit

---
+++ sb-ex1t1-pom/src/main/java/org/pratap/ws/model/Greeting.java
@@ -0,0 +1,30 @@
+package org.pratap.ws.model;
+
+import java.math.BigInteger;
+
+public class Greeting {
+	
+	private BigInteger id;
+	private String text;
+	
+	public Greeting() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BigInteger getId() {
+		return id;
+	}
+
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
---
+++ sb-ex1t1-pom/src/main/java/org/pratap/ws/web/api/GreetingController.java
@@ -0,0 +1,52 @@
+package org.pratap.ws.web.api;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.pratap.ws.model.Greeting;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	private static BigInteger nextId;
+	private static Map<BigInteger, Greeting> greetingMap;
+	
+	private static Greeting save(Greeting greeting){
+		
+		if(greetingMap == null){
+			greetingMap = new HashMap<BigInteger, Greeting>();
+			nextId = BigInteger.ONE;
+		}
+		greeting.setId(nextId);
+		greetingMap.put(nextId, greeting);
+		nextId = nextId.add(BigInteger.ONE);
+		return greeting;
+	}
+	
+	static{
+		Greeting g1 = new Greeting();
+		g1.setText("SATENDRA PRATAP SINGH");
+		save(g1);
+		
+		Greeting g2 = new Greeting();
+		g2.setText("UTKARSH DEWANGAN");
+		save(g2);
+		
+	}
+
+	@RequestMapping(value="/api/greetings", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	ResponseEntity<Collection<Greeting>> getGreetings(){
+		Collection<Greeting> greetings = greetingMap.values();
+		return new ResponseEntity<Collection<Greeting>>(greetings, HttpStatus.OK);
+	}
+	
+	
+}
